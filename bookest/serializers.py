ca252078d807da616e3b71aeaf6f3411d295d6d9
jankat <cankatguven@gmail.com>
2024-02-10 23:10:14 +0300

    db-ch

--- bookest/serializers.py
+++ bookest/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken 
 from django.contrib.auth.models import User
 from .models import Book, BookShelf
 
